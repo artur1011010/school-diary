41893b4f324a7174ed29eef43474b064c8ae39f9
Artur Zaczek <artur.zaczek@tlen.pl>
2021-05-02 01:02:08 +0200

    services refactoring, ResponseEntity

--- src/main/java/pl/arturzaczek/demoSchool/controllers/rest/GradeRestController.java
+++ src/main/java/pl/arturzaczek/demoSchool/controllers/rest/GradeRestController.java
@@ -3,15 +3,13 @@ package pl.arturzaczek.demoSchool.controllers.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.arturzaczek.demoSchool.model.dto.GradeDTO;
-import pl.arturzaczek.demoSchool.model.entities.Grade;
-import pl.arturzaczek.demoSchool.model.entities.User;
 import pl.arturzaczek.demoSchool.model.repositories.GradeRepository;
 import pl.arturzaczek.demoSchool.model.repositories.UserRepository;
 import pl.arturzaczek.demoSchool.service.GradeService;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest")
@@ -29,63 +27,18 @@ public class GradeRestController {
         this.gradeService = gradeService;
     }
 
-    @PostMapping("/addGradeRest")
-    public User addGradeRest() {
-        logger.debug("url= /rest/addGradeRest, method=addGradeRest()");
-        User user = new User("Artur", "Rest test");
-        user.addToGradeList(new Grade(80,"Matematyka", user.getId()));
-        userRepository.save(user);
-        return user;
-    }
-
-    @PostMapping("/addSecondGradeRest")
-    public User addSecondGradeRest() {
-        logger.debug("url= /rest/addSecondGradeRest, method=addSecondGradeRest()");
-        User resultUser = new User("Error", "wrong id");
-        Optional<User> byId = userRepository.findById(1L);
-        if (!byId.isPresent()) {
-            return resultUser;
-        }
-        resultUser = byId.get();
-        resultUser.addToGradeList(new Grade(50));
-        userRepository.save(resultUser);
-        resultUser.getGradeList().forEach(grade-> System.out.println("grade: " + grade));
-        return resultUser;
-    }
-
-    @PostMapping("/addGrade/{student}")
-    public User addGradeToStudentByIdTest(@PathVariable String student) {
-        logger.debug("url= /rest/addGrade/{student}, method=addGradeToStudentByIdTest(), STUDENT: " + student);
-        User resultUser = new User("Error", "wrong id");
-        Long id = null;
-        try {
-            id = Long.parseLong(student);
-        }catch (Exception exception){
-            logger.error("addGradeToStudentByIdTest() parse student id error: " +  student);
-            return resultUser;
-        }
-        Optional<User> byId = userRepository.findById(id);
-        if (!byId.isPresent()) {
-            return resultUser;
-        }
-        resultUser = byId.get();
-        resultUser.addToGradeList(new Grade(100));
-        userRepository.save(resultUser);
-        resultUser.getGradeList().forEach(grade-> System.out.println("grade: " + grade));
-        return resultUser;
-    }
-
     @PostMapping("/grade/{student}")
-    public void addGradeToStudentById(@PathVariable String student, @RequestBody GradeDTO gradeDTO) {
+    public ResponseEntity addGradeToStudentById(@PathVariable String student, @RequestBody GradeDTO gradeDTO) {
         logger.debug("url= /rest/grade/{student}, method=addGradeToStudentById(), STUDENT: " + student + " , GRADE: " + gradeDTO);
         Long id = null;
         try {
             id = Long.parseLong(student);
         }catch (Exception exception){
             logger.error("addGradeToStudentById() parse student id error: " +  student);
-            return;
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
         gradeService.addGradeToStudentById(id, gradeDTO);
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
 
--- src/main/java/pl/arturzaczek/demoSchool/controllers/rest/StudentRestController.java
+++ src/main/java/pl/arturzaczek/demoSchool/controllers/rest/StudentRestController.java
@@ -3,10 +3,13 @@ package pl.arturzaczek.demoSchool.controllers.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.arturzaczek.demoSchool.model.entities.User;
 import pl.arturzaczek.demoSchool.model.repositories.UserRepository;
 import pl.arturzaczek.demoSchool.service.StudentService;
+import pl.arturzaczek.demoSchool.service.UserService;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,33 +18,34 @@ import java.util.Optional;
 @RequestMapping("/rest")
 public class StudentRestController {
     UserRepository userRepository;
+    UserService userService;
     StudentService studentService;
     Logger logger = LoggerFactory.getLogger(StudentRestController.class);
 
     @Autowired
-    public StudentRestController(UserRepository userRepository, StudentService studentService) {
+    public StudentRestController(UserRepository userRepository, StudentService studentService,UserService userService) {
         this.userRepository = userRepository;
         this.studentService = studentService;
+        this.userService = userService;
     }
 
     @GetMapping("/students")
     public List<User> getStudents(){
         logger.debug("url= /rest/students, method=getStudents()");
-        return userRepository.findAll();
+        return userService.getUsersList();
     }
 
     @PostMapping("/student")
-    public void addStudent(@RequestBody User user){
+    public ResponseEntity addStudent(@RequestBody User user){
         logger.debug("url= /rest/student, method=addStudent() STUDENT: " + user);
-        userRepository.save(user);
+        userService.saveUser(user);
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/add20Students")
-    public void add20Students(){
-        List<User> randomUserM = studentService.createRandomUserM();
-        List<User> randomUserF = studentService.createRandomUserF();
-        randomUserM.addAll(randomUserF);
-        userRepository.saveAll(randomUserM);
+    public ResponseEntity add20Students(){
+        userService.save20users();
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/student/{student_id}")
@@ -58,15 +62,15 @@ public class StudentRestController {
     }
 
     @DeleteMapping("/student/{student_id}")
-    public String deleteStudentById(@PathVariable String student_id){
+    public ResponseEntity deleteStudentById(@PathVariable String student_id){
         logger.debug("url= /rest/student/{student_id}, method=deleteStudentById() STUDENT: " + student_id);
         Long long_id = Long.parseLong(student_id+"");
         Optional<User> byId = userRepository.findById(long_id);
         if(byId.isEmpty()){
             logger.warn("deleteStudentById() rest " + student_id);
-            return "not found: " + student_id;
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         userRepository.deleteById(long_id);
-        return "student deleted + " + long_id;
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
